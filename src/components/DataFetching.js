c98d1eaf06d2f0d3cf4daa4a280cfb88aa861134
Fajar Eko Susilo <fajarekos@Fajarekos.local>
2021-10-08 16:23:33 +0700

    Added: example code for transition on dropdown components

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import { Dropdown } from './dropdown/Dropdown'
 
 function DataFetching() {
   const [ posts, setPosts ] = useState([])
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then((res) => {
-        setPosts(res.data)
-      })
-      .catch((err) => {
-        console.error(err)
-      })
+    (async () => {
+      try {
+        const response = await axios('https://jsonplaceholder.typicode.com/posts')
+
+        setPosts(response.data)
+      } catch (error) {
+        console.error(error)
+      }
+    })();
+    // axios.get('https://jsonplaceholder.typicode.com/posts')
+    //   .then((res) => {
+    //     setPosts(res.data)
+    //   })
+    //   .catch((err) => {
+    //     console.error(err)
+    //   })
   },[])
   return (
     <div className="flex flex-wrap box-border px-3 py-2 border-gray-400">
       {
         posts.map(post => 
-          <div className="text-gray-600 text-leftgit px-3 py-2 w-1/5 space-x-2 border-2" key={post.id}>{ post.body }</div>
+          <div className="text-gray-600 text-leftgit px-3 py-2 w-1/5 space-x-2 border-2 m-2 text-left rounded" key={post.id}>
+            { post.body }
+            <Dropdown />
+          </div>
         )
       }
     </div>
